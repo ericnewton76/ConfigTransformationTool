1406f2bfc4b0221b1aa889cd4524ccf8c6cf91ef
crate-ssekoguchi <ssekoguchi@crateandbarrel.com>
2014-03-18 20:43:55 -0500

    added

---
+++ ConfigTransformationTool/OutputLog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace ConfigTransformationTool
+{
+    public class OutputLog
+    {
+        readonly TextWriter outputStream;
+        readonly TextWriter errorStream;
+
+        OutputLog(TextWriter outputStream, TextWriter errorStream)
+        {
+            this.outputStream = outputStream;
+            this.errorStream = errorStream;
+        }
+
+        public static OutputLog ErrorOnly(TextWriter errorStream)
+        {
+            if (errorStream == null)
+            {
+                throw new ArgumentNullException("errorStream");
+            }
+
+            return new OutputLog(null, errorStream);
+        }
+
+        public static OutputLog FromWriter(TextWriter writer, TextWriter errorStream)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            if (errorStream == null)
+            {
+                throw new ArgumentNullException("errorStream");
+            }
+
+            return new OutputLog(writer, errorStream);
+        }
+
+        public void WriteLine(string message, params object[] parameters)
+        {
+            if (this.outputStream != null)
+            {
+                this.outputStream.WriteLine(message, parameters);
+            }
+        }
+
+        public void WriteErrorLine(string message, params object[] parameters)
+        {
+            if (this.errorStream != null)
+            {
+                this.errorStream.WriteLine(message, parameters);
+            }
+        }
+    }
+}
